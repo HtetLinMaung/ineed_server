00913b36c4cf1bcdda6226e98ffa3441b483f94a
Htet Lin Maung <htet.lin.maung@fusionsol.com>
2020-09-02 13:00:55 +0630

    deploy

--- dist/middlewares/error.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (function (err, _req, res, _next) {
-    var message = err.message, data = err.data;
-    var statusCode = err.statusCode || 500;
-    res.status(statusCode).json({ message: message, data: data });
-});
-//# sourceMappingURL=error.js.map
