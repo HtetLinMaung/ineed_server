5040e9cc673cf05a0ea428d1ec7f239b453a5d40
Htet Lin Maung <jsthtet96@gmail.com>
2020-09-05 05:02:35 +0630

    update

--- src/utils.ts
+++ src/utils.ts
@@ -5,6 +5,6 @@ export const rootDir = path.join(__dirname, "..");
 
 export const deleteFile = (fileName: string) => {
   fs.unlink(path.join(rootDir, fileName), (err) => {
-    if (err) throw err;
+    if (err) console.log(err);
   });
 };
